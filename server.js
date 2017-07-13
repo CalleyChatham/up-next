f4ac4ad3e9983342c12ab501e2698964e31fd9e7
Samantha De Waal Malefyt <samanthadwm@gmail.com>
2017-07-12 21:01:37 -0500

    added server.js

---
+++ server.js
@@ -0,0 +1,33 @@
+// REQUIRING PACKAGES
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+var PORT = 5000;
+var app = express();
+
+mongoose.connect("mongodb://localhost/upnext");
+var db = mongoose.connection;
+
+db.on("error", function(err) {
+  console.log("Mongoose Error: ", err);
+});
+
+db.once("open", function() {
+  console.log("Mongoose connection successful.");
+});
+
+// HANDLE THE "/" ROUTE BEING REQUESTED BY THE USER
+app.get("/", function(req, res) {
+	res.send("Welcome to upNext");
+});
+
+require('./server/routes/customer')(app);
+require('./server/routes/menus')(app);
+require('./server/routes/orders')(app);
+require('./server/routes/trucks')(app);
+
+// THE HOUSE ADDRESS OF WHERE THE SERVER IS LOCATED WHEN INITIATED
+app.listen(PORT, function () {
+	console.log("Listening at port: " + PORT);
+});
