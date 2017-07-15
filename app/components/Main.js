06744ba004cc418059083a32b131804032b81ace
Tony P <typanh417@gmail.com>
2017-07-15 10:39:45 -0500

    update customer.js file

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,1 +1,13 @@
+// Include React
+var React = require("react");
 
+// Create the Customer Component
+var Customer = React.createClass({
+
+
+
+
+});
+
+// Export the component back for use in other files
+module.exports = Customer;
