b4bed0ff3df45894f1f54117c5864ed24e29f1f7
Samantha De Waal Malefyt <samanthadwm@gmail.com>
2017-07-17 19:09:59 -0500

    updated models

--- models/trucks.js
+++ models/trucks.js
@@ -9,7 +9,7 @@ var TruckSchema = new Schema({
 	fb_id: {
 		type: String,
 		required: true,
-		unique: true
+		unique: true,
 		validate: {
             validator: function (idOfTruck, cb) {
                 Truck.find({
