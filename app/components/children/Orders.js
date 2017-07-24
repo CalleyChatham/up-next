b2dc7416e24af49e654a410426b795ea551a87f7
Tony Panhhavong <Tony Panhhavong>
2017-07-24 18:48:19 -0500

    added orders

---
+++ app/components/children/Orders.js
@@ -0,0 +1,12 @@
+// Include React
+var React = require("react");
+// Including the Link component from React Router to navigate within our application without full page reloads
+var Link = require("react-router").Link;
+
+
+
+
+
+
+
+module.exports = Orders;
